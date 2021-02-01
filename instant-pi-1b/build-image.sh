3f59b32c3254e12418d62163f4634347a8d524d9
Iron Oxidizer <ironoxidizer@gmail.com>
2021-01-31 20:58:47 -0500

    Fixed conditional download and shebang

--- instant-pi-1b/build-image.sh
+++ instant-pi-1b/build-image.sh
@@ -1,4 +1,6 @@
-if [ $1 != "-N" ]; then
+#!/bin/sh
+
+if [ $# -eq 0 ] || [ $1 != "-N" ]; then
     curl https://buildroot.org/downloads/buildroot-2020.08.1.tar.bz2 | tar xj && \
     wget -N https://github.com/raspberrypi/firmware/raw/master/boot/bcm2708-rpi-b.dtb -P buildroot-2020.08.1/output/images/
     wget -N https://github.com/raspberrypi/firmware/raw/master/boot/bootcode.bin -P buildroot-2020.08.1/output/images/
